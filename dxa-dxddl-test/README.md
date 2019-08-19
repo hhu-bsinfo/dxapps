1ea98232a663bff5258a56b67a785baaf355cf88
Michael Schoettner <michael.schoettner@uni-duesseldorf.de>
2019-08-19 14:40:02 +0200

    added dxddl demo

---
+++ dxa-dxddl-test/README.md
@@ -0,0 +1,8 @@
+# DXApp hello world example and template
+This application provides a minimal hello world example to serve as a template for developing applications (dxapps) for
+[DXRAM](https://github.com/hhu-bsinfo/dxram/). Further documentation on how to develop applications for DXRAM can be
+found in the docs of the [DXRAM repository](https://github.com/hhu-bsinfo/dxram/).
+
+# License
+Copyright (C) 2018 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems.
+Licensed under the [GNU General Public License](LICENSE.md).
---
+++ dxa-dxddl-test/src/main/java/de/hhu/bsinfo/dxapp/HelloApplication.java
@@ -0,0 +1,45 @@
+package de.hhu.bsinfo.dxapp;
+
+import java.util.Arrays;
+
+import de.hhu.bsinfo.dxram.app.Application;
+import de.hhu.bsinfo.dxram.boot.BootService;
+import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
+import de.hhu.bsinfo.dxram.generated.BuildConfig;
+import de.hhu.bsinfo.dxutils.NodeID;
+
+/**
+ * "Hello world" example DXRAM application.
+ *
+ * @author Stefan Nothaas, stefan.nothaas@hhu.de, 17.05.17
+ */
+public class HelloApplication extends Application {
+    @Override
+    public DXRAMVersion getBuiltAgainstVersion() {
+        return BuildConfig.DXRAM_VERSION;
+    }
+
+    @Override
+    public String getApplicationName() {
+        return "HelloApplication";
+    }
+
+    @Override
+    public void main(final String[] p_args) {
+        BootService bootService = getService(BootService.class);
+
+        System.out.printf("\n");
+        System.out.printf("  Hello! I am %s running on node %s.\n", getApplicationName(), NodeID.toHexStringShort(bootService.getNodeID()));
+        System.out.printf("  My arguments are: %s\n", Arrays.toString(p_args));
+        System.out.printf("\n");
+
+        // Put your application code running on the DXRAM node/peer here
+    }
+
+    @Override
+    public void signalShutdown() {
+        // Interrupt any flow of your application and make sure it shuts down.
+        // Do not block here or wait for something to shut down. Shutting down of your application
+        // must be execute asynchronously
+    }
+}
