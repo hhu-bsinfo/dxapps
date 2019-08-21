fff44f6cbd4c9939dacaf5ffdca8a734840b2250
Michael Schoettner <michael.schoettner@uni-duesseldorf.de>
2019-08-21 14:58:26 +0200

    update

--- dxa-dxddl-demo/src/main/java/de/hhu/bsinfo/dxapp/DxddlDemoApplication.java
+++ dxa-dxddl-demo/src/main/java/de/hhu/bsinfo/dxapp/DxddlDemoApplication.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 import de.hhu.bsinfo.dxram.app.Application;
 import de.hhu.bsinfo.dxram.boot.BootService;
+import de.hhu.bsinfo.dxram.nameservice.NameserviceService;
 import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
 import de.hhu.bsinfo.dxram.generated.BuildConfig;
 import de.hhu.bsinfo.dxutils.NodeID;
+import de.hhu.bsinfo.dxram.ms.*;
 
 /**
  * "DXDDL Demo" example DXRAM application.
@@ -26,11 +28,41 @@ public class DxddlDemoApplication extends Application {
 
     @Override
     public void main(final String[] p_args) {
-        BootService bootService = getService(BootService.class);
+        BootService bootService = getService( BootService.class );
+        NameserviceService nameService = getService( NameserviceService.class );
+        MasterSlaveComputeService computeService = getService( MasterSlaveComputeService.class );
+        String chunkName = "1";
 
         System.out.printf("\n");
-        System.out.printf("  DxddlDemoApplication\n");
-        System.out.printf("\n");
+        System.out.printf("  DxddlDemoApplication\n\n"");
+
+        if ( computeService.getComputeRole() == ComputeRole.MASTER ) {
+           GenerateDataTask generateDataTask = new GenerateDataTask();
+           TaskScript generateDataTaskScript = new TaskScript( generateDataTask );
+           TaskScriptState inputState = computeService.submitTaskScript( generateDataTaskScript, (short) 0);
+
+           while (!inputState.hasTaskCompleted()) {
+              try {
+                   Thread.sleep(100);
+              } catch (final InterruptedException ignore) {
+
+              }
+           }
+        }
+
+
+        System.out.println("Register chunk " + chunkName + " in NameService");
+        nameService.register(100, chunkName);
+
+        System.out.println("Lookup entry " + chunkName +" in NameService");
+        long result = nameService.getChunkID(chunkName, 1000);
+        System.out.println("   returned chunkID = " + result);
+
+        System.out.println("ComputeRole");
+        if ( computeService.getComputeRole() == ComputeRole.MASTER )
+           System.out.println("   Master");
+       else
+           System.out.println("    Slave");
 
         // Put your application code running on the DXRAM node/peer here
     }
