d54debea7dd389aac1d6ef2e8cfabd2848d53624
Michael Schoettner <michael.schoettner@uni-duesseldorf.de>
2019-08-21 18:26:57 +0200

    update

--- dxa-dxddl-demo/src/main/java/de/hhu/bsinfo/dxapp/chunks/RootChunk.java
+++ dxa-dxddl-demo/src/main/java/de/hhu/bsinfo/dxapp/chunks/RootChunk.java
@@ -3,7 +3,7 @@ package de.hhu.bsinfo.dxapp.chunks;
 import de.hhu.bsinfo.dxmem.data.AbstractChunk;
 import de.hhu.bsinfo.dxutils.serialization.Exporter;
 import de.hhu.bsinfo.dxutils.serialization.Importer;
-
+import java.util.Random;
 
 /**
  * Chunk to store root info to be accessed by master
@@ -12,32 +12,44 @@ import de.hhu.bsinfo.dxutils.serialization.Importer;
 public class RootChunk extends AbstractChunk {
 
     private int m_mySlaveIndex;
+    private int m_dummy;
 
     public RootChunk(int mySlaveIndex) {
         super();
+
+        Random rand = new Random();
         m_mySlaveIndex = mySlaveIndex;
+        m_dummy = rand.nextInt(32768);
     }
 
     public double getMySlaveIndex() {
         return m_mySlaveIndex;
     }
+    public double getDummy() {
+        return m_dummy;
+    }
 
     public void setMySlaveIndex (int mySlaveIndex) {
         m_mySlaveIndex = mySlaveIndex;
+    }
+    public void setDummy (int val) {
+        m_dummy = val;
     }
 
     @Override
     public void exportObject(Exporter p_exporter) {
         p_exporter.writeInt(m_mySlaveIndex);
+        p_exporter.writeInt(m_dummy);
     }
 
     @Override
     public void importObject(Importer p_importer) {
         m_mySlaveIndex = p_importer.readInt(m_mySlaveIndex);
+        m_dummy = p_importer.readInt(m_dummy);
     }
 
     @Override
     public int sizeofObject() {
-        return Integer.BYTES;
+        return 2*Integer.BYTES;
     }
 }
