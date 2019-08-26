c9a459fa74097796f644854287459c3ddb8bfc17
Michael Schoettner <michael.schoettner@uni-duesseldorf.de>
2019-08-26 10:36:59 +0200

    update

--- dxa-masterslave/src/main/java/de/hhu/bsinfo/dxapp/chunks/HeadChunk.java
+++ dxa-masterslave/src/main/java/de/hhu/bsinfo/dxapp/chunks/HeadChunk.java
@@ -5,38 +5,38 @@ import de.hhu.bsinfo.dxutils.serialization.Exporter;
 import de.hhu.bsinfo.dxutils.serialization.Importer;
 
 /**
- * Chunk to store root info accessed by master
+ * Chunk to store head info accessed by master
  */
-public class RootChunk extends AbstractChunk {
-    private long m_root_NodeChunk;      // ChunkID of root entry of list (type NodeChunk)
+public class HeadChunk extends AbstractChunk {
+    private long m_head_NodeChunk;      // ChunkID of root entry of list (type NodeChunk)
     private int  m_sum;                 // sum of all entries in list referenced by m_root_NodeChunk
 
     // needed for creating new chunk
-    public RootChunk() { }
+    public HeadChunk() { }
 
     // needed for creating new chunk
-    public RootChunk(long root, int sum) {
+    public HeadChunk(long root, int sum) {
         super();
 
-        m_root_NodeChunk = root;
+        m_head_NodeChunk = root;
         m_sum = sum;
     }
 
     // needed for reading/writing existing object from/to DXMem
-    public RootChunk(long p_chunkID) {
+    public HeadChunk(long p_chunkID) {
         super(p_chunkID);
     }
 
-    public long getRoot() {
-        return m_root_NodeChunk;
+    public long getHead() {
+        return m_head_NodeChunk;
     }
 
     public int getSum() {
         return m_sum;
     }
 
-    public void setRoot (long root) {
-        m_root_NodeChunk = root;
+    public void setHead (long head) {
+        m_head_NodeChunk = head;
     }
 
     public void setSum (int sum) {
@@ -45,13 +45,13 @@ public class RootChunk extends AbstractChunk {
 
     @Override
     public void exportObject(Exporter p_exporter) {
-        p_exporter.writeLong(m_root_NodeChunk);
+        p_exporter.writeLong(m_head_NodeChunk);
         p_exporter.writeInt(m_sum);
     }
 
     @Override
     public void importObject(Importer p_importer) {
-        m_root_NodeChunk = p_importer.readLong(m_root_NodeChunk);
+        m_head_NodeChunk = p_importer.readLong(m_head_NodeChunk);
         m_sum = p_importer.readInt(m_sum);
     }
 
